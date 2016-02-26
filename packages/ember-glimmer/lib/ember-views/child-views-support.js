29385a4c96d5437ba7ca6f91c395ea678eab5f7c
Godfrey Chan <godfreykfc@gmail.com>
2016-02-25 17:34:21 -0800

    Do less work when creating Components in glimmer
    We starting noticing that there are quite a bit of work we do in
    Components init that are not needed in Glimmer, so we tried to make a
    slimmer version. When we eventually get to feature-flagging, we can
    conditionally export the right class. We will probably need to add more
    stuff as we go (or perhaps we can remove more work), but this is a good
    start.

---
+++ packages/ember-glimmer/lib/ember-views/child-views-support.js
@@ -0,0 +1,73 @@
+/**
+@module ember
+@submodule ember-views
+*/
+import { Mixin } from 'ember-metal/mixin';
+import { get } from 'ember-metal/property_get';
+import { getOwner, setOwner, OWNER } from 'container/owner';
+
+export default Mixin.create({
+  init() {
+    this._super(...arguments);
+
+    /**
+      Array of child views. You should never edit this array directly.
+      Instead, use `appendChild` and `removeFromParent`.
+
+      @property childViews
+      @type Array
+      @default []
+      @private
+    */
+    this.childViews = [];
+    this.ownerView = this.ownerView || this;
+  },
+
+  appendChild(view) {
+    this.linkChild(view);
+    this.childViews.push(view);
+  },
+
+  destroyChild(view) {
+    view.destroy();
+  },
+
+  /**
+    Removes the child view from the parent view.
+
+    @method removeChild
+    @param {Ember.View} view
+    @return {Ember.View} receiver
+    @private
+  */
+  removeChild(view) {
+    // If we're destroying, the entire subtree will be
+    // freed, and the DOM will be handled separately,
+    // so no need to mess with childViews.
+    if (this.isDestroying) { return; }
+
+    // update parent node
+    this.unlinkChild(view);
+
+    // remove view from childViews array.
+    var childViews = get(this, 'childViews');
+
+    var index = childViews.indexOf(view);
+    if (index !== -1) { childViews.splice(index, 1); }
+
+    return this;
+  },
+
+  linkChild(instance) {
+    if (!instance[OWNER]) {
+      setOwner(instance, getOwner(this));
+    }
+
+    instance.parentView = this;
+    instance.ownerView = this.ownerView;
+  },
+
+  unlinkChild(instance) {
+    instance.parentView = null;
+  }
+});
